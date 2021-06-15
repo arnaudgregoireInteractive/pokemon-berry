5003b13df45a8b6b1b6a85394187079f807ef591
arnaudgregoire <arnaudgregoire92@gmail.com>
2021-06-15 11:36:49 +0200

    without helmet

--- app/index.js
+++ app/index.js
@@ -9,8 +9,9 @@ const app = express();
 const httpServer = createServer(app);
 const gameServer = new Server({ server: httpServer });
 
+//console.log(process.env);
 // Express middleware
-app.use(helmet({ contentSecurityPolicy: process.env.NODE_ENV === 'production' }));
+//app.use(helmet({ contentSecurityPolicy: false })); // process.env.NODE_ENV === 'production'
 app.use(express.json());
 app.use(express.static(join(__dirname, 'client', 'dist')));
 
@@ -21,19 +22,16 @@ app.get('/', (req, res) => {
 
 // Colyseus room
 const { ZONES } = require('./shared/enum');
-const PalletTownRoom = require('./room/pallet-town-room');
-const HeroHome1fRoom = require('./room/hero_home_1f-room');
-const HeroHome2fRoom = require('./room/hero_home_2f-room');
-const RivalHome1fRoom = require('./room/rival_home_1f-room');
-const OakLab1fRoom = require('./room/oak_lab_1f-room');
-const Route1Room = require('./room/route1-room');
+const GameRoom = require('./room/game-room');
 
-gameServer.define(ZONES.PALLET_TOWN, PalletTownRoom);
-gameServer.define(ZONES.HERO_HOME_1F, HeroHome1fRoom);
-gameServer.define(ZONES.HERO_HOME_2F, HeroHome2fRoom);
-gameServer.define(ZONES.RIVAL_HOME_1F, RivalHome1fRoom);
-gameServer.define(ZONES.OAKS_LAB_1F, OakLab1fRoom);
-gameServer.define(ZONES.ROUTE1, Route1Room);
+Object.keys(ZONES).forEach(zone => {
+  maclass = class extends GameRoom{
+    constructor(){
+        super(zone);
+    }
+  }
+  gameServer.define(zone, maclass);
+});
 
 // Start
 gameServer.listen(port).then(() => {
