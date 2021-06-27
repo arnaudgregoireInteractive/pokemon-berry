85fc4679496d81a7b39b72e7097c3ddf512afa70
arnaudgregoire <arnaudgregoire92@gmail.com>
2021-06-27 21:18:57 +0200

    remove fb auth

--- app/client/src/component/pages/auth.jsx
+++ app/client/src/component/pages/auth.jsx
@@ -28,8 +28,7 @@ class Auth extends Component {
           signInFlow: 'popup',
           // We will display Google and Facebook as auth providers.
           signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
           ],
           callbacks: {
             // Avoid redirects after sign-in.
