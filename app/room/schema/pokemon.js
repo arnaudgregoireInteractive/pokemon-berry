8aacbfa225007191d54535214d1ff4d4571727e8
arnaudgregoire <arnaudgregoire92@gmail.com>
2021-06-28 21:53:38 +0200

    pokemon start

--- app/room/schema/pokemon.js
+++ app/room/schema/pokemon.js
@@ -1,8 +1,9 @@
 const schema = require('@colyseus/schema');
+const { POKEMON_NAME, RARITY, POKEMON_TYPE } = require('../../shared/enum');
 const Schema = schema.Schema;
 
 class Pokemon extends Schema {
-  constructor(id, name, x, y, orientation, status, power, types) {
+  constructor(id, x, y, orientation, status, power, name, rarity, types) {
     super();
     this.assign({
         id: id,
@@ -12,6 +13,7 @@ class Pokemon extends Schema {
         orientation: orientation,
         status: status,
         power: power,
+        rarity: rarity
     });
     this.types = schema.ArraySchema();
     types.forEach(t => {
@@ -28,7 +30,8 @@ class Pokemon extends Schema {
         orientation: this.orientation,
         status: this.status,
         power: this.power,
-        types: this.types
+        types: this.types,
+        rarity: this.rarity
     }
   }
 }
@@ -40,8 +43,15 @@ schema.defineTypes(Pokemon, {
   y: 'uint8',
   orientation: 'string',
   status: 'string',
+  rarity: 'string',
   power: 'uint16',
   types: ['string']
 });
 
-module.exports = Pokemon;
+class Rattata extends Pokemon{
+    constructor(id, x, y, orientation, status, power){
+        super(id, x, y, orientation, status, power, POKEMON_NAME.RATTATA, RARITY.COMMON, [POKEMON_TYPE.NORMAL]);
+    }
+}
+
+module.exports = {Pokemon, Rattata};
